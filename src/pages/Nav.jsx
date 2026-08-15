b89f711f5ec58fd8b3f5875b646e56cde7c5c517
BFUR64 <iterranceevan@gmail.com>
2026-08-15 14:16:15 +0800

    refactor: Add nav style on select

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 import './css/nav.css';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Nav () {
     return (
@@ -10,12 +10,47 @@ export default function Nav () {
             <img src="./logo.png" />
 
             <ul>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/classes">Classes</Link></li>
-                <li><Link to="/quizzes">Quizzes</Link></li>
-                <li><Link to="/reports">Reports</Link></li>
-                <li><Link to="/lesson_plans">Lesson Plans</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
+                <li>
+                    <NavLink
+                        to="/dashboard"
+                        className={({ isActive }) => isActive ? "active" : ""}
+                    >Dashboard</NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/classes"
+                        className={({ isActive }) => isActive ? "active" : ""}
+                    >Classes</NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/quizzes"
+                        className={({ isActive }) => isActive ? "active" : ""}
+                    >Quizzes</NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/reports"
+                        className={({ isActive }) => isActive ? "active" : ""}
+                    >Reports</NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/lesson_plans"
+                        className={({ isActive }) => isActive ? "active" : ""}
+                    >Lesson Plans</NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/settings"
+                        className={({ isActive }) => isActive ? "active" : ""}
+                    >Settings</NavLink>
+                </li>
             </ul>
         </header>
     );
